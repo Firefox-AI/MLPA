06da3a544dd52908493ae818405bbf28d7489f55
noahpodgurski <noahpodgurski@gmail.com>
2025-10-31 09:08:58 -0400

    fix: update app_attest DB scripts for deployment

--- scripts/migrate-app-attest-database.sh
+++ scripts/migrate-app-attest-database.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
 APP_ATTEST_DATABASE_URL="postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${APP_ATTEST_DB_NAME}"
+echo "Using APP_ATTEST_DATABASE_URL: ${APP_ATTEST_DATABASE_URL}"
+# Create the app_attest database if it doesn't exist
+psql "postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/postgres" \
+    -c "SELECT 1 FROM pg_database WHERE datname='${APP_ATTEST_DB_NAME}';" | grep -q 1 || \
+    psql "postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/postgres" \
+    -c "CREATE DATABASE \"${APP_ATTEST_DB_NAME}\";"
+
 alembic -x sqlalchemy.url="${APP_ATTEST_DATABASE_URL}" upgrade head
