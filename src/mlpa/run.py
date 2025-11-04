763cca7853d4d13f45e5152019bb5146c34d82d5
Alexander Osipenko <11722602+subpath@users.noreply.github.com>
2025-11-04 14:36:27 +0100

    use 2 workers

--- src/mlpa/run.py
+++ src/mlpa/run.py
@@ -120,7 +120,7 @@ async def chat_completion(
 
 
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=env.PORT, timeout_keep_alive=10)
+    uvicorn.run(app, host="0.0.0.0", port=env.PORT, timeout_keep_alive=10, workers=2)
 
 
 if __name__ == "__main__":
