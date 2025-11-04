78416cf2028a6b52737b7ec38c09cd77b1f53cb3
Alexander Osipenko <11722602+subpath@users.noreply.github.com>
2025-11-04 18:07:39 +0100

    using default amount of threads

--- src/mlpa/run.py
+++ src/mlpa/run.py
@@ -2,7 +2,7 @@ import time
 from contextlib import asynccontextmanager
 from typing import Annotated, Optional
 
-import anyio.to_thread
+# import anyio.to_thread
 import sentry_sdk
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, Request, Response
@@ -36,8 +36,8 @@ tags_metadata = [
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # NOTE: From https://starlette.dev/threadpool/
-    limiter = anyio.to_thread.current_default_thread_limiter()
-    limiter.total_tokens = 100
+    # limiter = anyio.to_thread.current_default_thread_limiter()
+    # limiter.total_tokens = 40
     await litellm_pg.connect()
     await app_attest_pg.connect()
     yield
